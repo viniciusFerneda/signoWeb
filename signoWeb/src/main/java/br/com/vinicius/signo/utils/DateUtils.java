275579fdd463f652042e657ae0d5901cf411e26a
Noturno <Noturno@192.168.7.20>
2017-09-14 21:56:53 -0300

    metodos time

--- signoWeb/src/main/java/br/com/vinicius/signo/utils/DateUtils.java
+++ signoWeb/src/main/java/br/com/vinicius/signo/utils/DateUtils.java
@@ -7,6 +7,7 @@ import java.util.Date;
 public final class DateUtils {
 
 	public static final String PATTERN_DATA_PADRAO = "dd/MM/yyyy";
+	public static final String PATTERN_TEMPO_PADRAO = "hh:mm";
 
 	private DateUtils() {
 		throw new UnsupportedOperationException();
@@ -25,6 +26,21 @@ public final class DateUtils {
 	public static String formatData(Date data, String pattern) {
 		SimpleDateFormat sdf = new SimpleDateFormat(pattern);
 		return sdf.format(data);
+	}
+	
+	public static Date parseTempo(String tempo, String pattern) {
+		SimpleDateFormat sdf = new SimpleDateFormat(pattern);
+		try {
+			return sdf.parse(tempo);
+		} catch (ParseException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	public static String formatTempo(Date tempo, String pattern) {
+		SimpleDateFormat sdf = new SimpleDateFormat(pattern);
+		return sdf.format(tempo);
 	}
 
 }
